beb7c593ecfe0d5ce9a719615f4e58127e78828b
bartek szurgot <el-bart@tweety>
2010-02-27 16:10:12 +0100

    sample implementation.
    modified:
      ppmp/PPMP/ppmp.cpp

--- ppmp/PPMP/ppmp.cpp
+++ ppmp/PPMP/ppmp.cpp
@@ -5,22 +5,31 @@
 #include <iostream>
 
 #include "Common/Exception.hpp"
-#include "PPMP/Mangling/Mangler.hpp"
 #include "PPMP/Reader.hpp"
 #include "PPMP/Writer.hpp"
+#include "PPMP/Mangling/Mangler.hpp"
+#include "PPMP/Mangling/Lowercase.hpp"
+#include "PPMP/Mangling/Uppercase.hpp"
+#include "PPMP/Mangling/NumberAdder.hpp"
 
 using namespace std;
+using namespace Common;
 using namespace PPMP;
+using namespace PPMP::Mangling;
 
 int main(int /*argc*/, char **argv)
 {
   try
   {
-    Reader rd(cin);
-    Writer wr(cout);
-
-
+    Reader       rd(cin);
+    Writer       wr(cout);
+    NumberAdder  na(wr, Range(0, 9) );
+    Processor   &p=na;
     // TODO
+
+    FastString str;
+    while( rd.read(str) )
+      p.process(str);
   }
   catch(const Common::Exception &ex)
   {
