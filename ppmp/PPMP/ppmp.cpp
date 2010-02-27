b9fada69de42a80bc5f7671a0880ae43cab0ab20
bartek szurgot <el-bart@tweety>
2010-02-27 12:55:08 +0100

    initial sets.
    modified:
      ppmp/PPMP/ppmp.cpp

--- ppmp/PPMP/ppmp.cpp
+++ ppmp/PPMP/ppmp.cpp
@@ -1,4 +1,21 @@
+/*
+ * ppmp.cpp
+ *
+ */
+#include <iostream>
+
+#include "PPMP/Mangling/Mangler.hpp"
+#include "PPMP/Reader.hpp"
+#include "PPMP/Writer.hpp"
+
+using namespace PPMP;
+
 int main(void)
 {
+  Reader rd(std::cin);
+  Writer wr(std::cout);
+
+  // TODO
+
   return 0;
 }
