c1018c925dfc4c68233979fbdeafd4141c3d6068
bartek szurgot <el-bart@tweety>
2010-02-27 11:53:03 +0100

    initial faststring's implementation.
    added:
      common/Common/FastString.hpp
      common/Common/FastString.t.cpp

---
+++ common/Common/FastString.hpp
@@ -0,0 +1,69 @@
+/*
+ * FastString.hpp
+ *
+ */
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+
+/* public header */
+
+namespace Common
+{
+
+class FastString
+{
+public:
+  FastString(void)
+  {
+    data_[0]=0;
+  }
+
+  FastString(const char *str)
+  {
+    if(str==NULL)
+      data_[0]=0;
+    else
+    {
+      strncpy(data_, str, sizeof(data_) );
+      data_[maxSize()]=0;   // ensure NULL-termination
+    }
+  }
+
+  char operator[](unsigned char pos) const
+  {
+    assertPos(pos);
+    return data_[pos];
+  }
+
+  char &operator[](unsigned char pos)
+  {
+    assertPos(pos);
+    return data_[pos];
+  }
+
+  const char *c_str(void) const
+  {
+    return data_;
+  }
+
+  size_t size(void) const
+  {
+    return strlen(data_);
+  }
+
+  static size_t maxSize(void)
+  {
+    return sizeof(data_)-1;
+  }
+
+private:
+  void assertPos(unsigned char pos) const
+  {
+    assert( pos<=maxSize() && "requested index out of scope" );
+  }
+
+  char data_[64+1];   // max password length
+}; // class FastString
+
+} // namespace Common
---
+++ common/Common/FastString.t.cpp
@@ -0,0 +1,97 @@
+/*
+ * FastString.t.cpp
+ *
+ */
+#include <tut/tut.hpp>
+
+#include "Common/FastString.hpp"
+#include <string>
+
+using namespace Common;
+
+namespace
+{
+struct TestClass
+{
+  void check(const FastString &out, const std::string &expected) const
+  {
+    tut::ensure_equals("invalid output string", out.c_str(), expected);
+  }
+};
+
+typedef tut::test_group<TestClass> factory;
+typedef factory::object            testObj;
+
+factory tf("Common/FastString");
+} // unnamed namespace
+
+
+namespace tut
+{
+
+// test default c-tor
+template<>
+template<>
+void testObj::test<1>(void)
+{
+  FastString fs;
+  check(fs, "");
+}
+
+// test c-tor with paramter
+template<>
+template<>
+void testObj::test<2>(void)
+{
+  const FastString fs("alice has a cat");
+  check(fs, "alice has a cat");
+}
+
+// test too long string in c-tor
+template<>
+template<>
+void testObj::test<3>(void)
+{
+  const FastString fs("01234567890123456789012345678901234567890123456789012345678901234");
+  check(fs, "0123456789012345678901234567890123456789012345678901234567890123"); // note: last dig truncated
+}
+
+// check max-size
+template<>
+template<>
+void testObj::test<4>(void)
+{
+  ensure_equals("invalid max size", FastString::maxSize(), 64);
+}
+
+// check size
+template<>
+template<>
+void testObj::test<5>(void)
+{
+  const FastString fs("abc");
+  ensure_equals("invalid size", fs.size(), 3);
+}
+
+// check non-const []
+template<>
+template<>
+void testObj::test<6>(void)
+{
+  FastString fs("Abc");
+  fs[2]='C';
+  check(fs, "AbC");
+}
+
+// check const []
+template<>
+template<>
+void testObj::test<7>(void)
+{
+  const FastString fs("abc");
+  ensure_equals("invalid char 1", fs[0], 'a');
+  ensure_equals("invalid char 2", fs[1], 'b');
+  ensure_equals("invalid char 3", fs[2], 'c');
+}
+
+} // namespace tut
